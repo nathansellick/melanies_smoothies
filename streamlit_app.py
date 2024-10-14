2a6695ca6edbff89427f55e55ce076914df4156d
nathansellick <nsellick3@gmail.com>
2024-10-14 20:12:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
-        st.success(f'Your Smoothie is ordered {name_on_order}!' ,icon="✅)
+        st.success(f'Your Smoothie is ordered {name_on_order}!' ,icon="✅")
